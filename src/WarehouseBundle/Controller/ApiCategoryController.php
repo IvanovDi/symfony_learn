b9f4bdaddcbcf32736df0c143baa68f0c0a36dfd
IvanovDi <dima77703011983@gmail.com>
2018-02-16 16:59:17 +0200

    added put and patch and delete method

--- src/WarehouseBundle/Controller/ApiCategoryController.php
+++ src/WarehouseBundle/Controller/ApiCategoryController.php
@@ -5,6 +5,7 @@ namespace WarehouseBundle\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,17 +32,15 @@ class ApiCategoryController extends FOSRestController
 
         }
 
-        $view = $this->view($articles)
+        $view = $this->view($articles, 200)
         ->setFormat('json');
 
         return $this->handleView($view);
     }
 
 
-    public function getCategoryAction($category)
+    public function getCategoryAction(Category $category)
     {
-        $category = $this->getDoctrine()->getRepository('WarehouseBundle:Category')->find($category);
-
         $view = $this->view($category)
             ->setFormat('json');
 
@@ -85,14 +84,63 @@ class ApiCategoryController extends FOSRestController
 
     }
 
-    public function putCategoryAction($category, Request $request)
+    public function putCategoryAction(Category $category, Request $request)
     {
-        $category = $this->getDoctrine()->getRepository('WarehouseBundle:Category')->find($category);
+        $form = $this->createForm(CategoryForm::class, $category, ['csrf_protection' => false, 'method' => $request->getMethod()]);
+
+        return $this->processForm($request, $form);
+
+    }
 
 
-        $response = new Response();
-        $response->setStatusCode(200);
+    public function patchCategoryAction(Category $category, Request $request)
+    {
+        $form = $this->createForm(CategoryForm::class, $category, ['csrf_protection' => false, 'method' => $request->getMethod()]);
 
-        return $response;
+        return $this->processForm($request, $form);
+    }
+
+    public function deleteCategoryAction(Category $category)
+    {
+        if($category) {
+             $em = $this->getDoctrine()->getManager();
+             $em->remove($category);
+             $em->flush();
+        }
+
+        return new Response(null, 204);
+    }
+
+    protected function processForm(Request $request, FormInterface $form)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $clearMissing = $request->getMethod() != 'PATCH';
+
+        $form->submit($data, $clearMissing);
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($form->getData());
+
+        if (count($errors) == 0 && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->flush();
+
+            $response = new Response();
+            $response->setStatusCode(200);
+
+            return $response;
+
+        } else {
+
+            $response = new Response();
+            $response->setStatusCode(400);
+            $response->setContent($errors);
+
+            return $response;
+
+        }
     }
 }
